29dfc03c128b5e9887d75665961af0e1c83edef5
Lucas Fernandes <lucas.zero55@hotmail.com>
2025-08-15 22:16:42 -0300

    feat: adding logo and pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,6 +11,42 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      injectRegister: 'auto',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      manifest: {
+        name: "Cânticos Mocidade",
+        short_name: "Cânticos",
+        description: "App de letras dos cânticos da mocidade",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icone-192x192.png", // <== don't add slash, for testing
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icone-512x512.png", // <== don't remove slash, for testing
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icone-512x512.png", // <== don't add slash, for testing
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
